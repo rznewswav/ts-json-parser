3189b45daa6d8b4750399486230857293f9428c5
raflie <raflie@newswav.com>
2023-06-15 17:14:42 +0800

    Use seek to try parse json

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { parseValue } from './parse-value';
 import { StringReader } from './string-reader';
 
 const input = `
-{ "hello": "world"  , "array": [1, "234", false ]}
+here is a sample json
+{ "hello": "world"  , "array": [1, "234", false, {} ]}
 
 Note: this is an example of a json file`;
 const sr = new StringReader(input);
-sr.skipWhitespaces();
-console.log(parseValue(sr));
-console.log('Left:', sr.slice());
+sr.seek('{');
+console.dir(parseValue(sr), { depth: null });
+console.log();
+console.log('Leftover string:', sr.slice());
